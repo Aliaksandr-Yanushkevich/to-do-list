17542412d0d093013e7973e4ab36a67609b8e7d2
“Aliaksandr <aliaksandr.yanushkevich@gmail.com>
2020-02-26 03:24:48 +0300

    fix: correct date display

--- src/util/dateUtil.js
+++ src/util/dateUtil.js
@@ -1,3 +1,8 @@
 export const formatDate = (date) => {
-  return `${date.getHours()}:${date.getMinutes()} ${date.getDate()}.${date.getMonth() + 1}.${date.getFullYear()}`;
+  const hours = ('0' + date.getHours()).slice(-2);
+  const minutes = ('0' + date.getMinutes()).slice(-2);
+  const seconds = ('0' + date.getSeconds()).slice(-2);
+  const day = ('0' + (date.getDate() + 1)).slice(-2);
+  const months = ('0' + (date.getMonth() + 1)).slice(-2);
+  return `${hours}:${minutes}:${seconds} ${day}.${months}.${date.getFullYear()}`;
 }
