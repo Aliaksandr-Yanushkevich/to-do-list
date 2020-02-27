ff748310f0d804d4d23ad92888248f75c5056bd7
“Aliaksandr <aliaksandr.yanushkevich@gmail.com>
2020-02-28 00:06:21 +0300

    fix: change eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,24 +1,19 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "es6": true
-    },
-    "extends": "airbnb",
-    "globals": {
-        "Atomics": "readonly",
-        "SharedArrayBuffer": "readonly"
-    },
-    "parserOptions": {
-        "ecmaVersion": 2018,
-        "sourceType": "module"
-    },
-    "rules": {
-      "import/no-extraneous-dependencies": ["error", {"devDependencies": true}],
-      "import/no-named-as-default": 0,
-      "class-methods-use-this": 0,
-      "no-console": 0,
-      "no-continue": 0,
-      "max-len": ["error", { "code": 120 }],
-      "quote-props": 0
-    }
+  "env": {
+    "browser": true,
+    "es6": true
+  },
+  "extends": "airbnb",
+  "globals": {
+    "Atomics": "readonly",
+    "SharedArrayBuffer": "readonly"
+  },
+  "parserOptions": {
+    "ecmaVersion": 2018,
+    "sourceType": "module"
+  },
+  "rules": {
+    "max-len": ["error", { "code": 120 }],
+    "no-param-reassign": 0
+  }
 };
